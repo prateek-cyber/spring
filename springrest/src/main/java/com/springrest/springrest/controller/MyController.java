20158d9b25ab6572c01561a751bdb4e601243ddc
prateek-cyber <67386701+prateek-cyber@users.noreply.github.com>
2022-03-14 14:58:18 +0530

    Add files via upload

---
+++ springrest/src/main/java/com/springrest/springrest/controller/MyController.java
@@ -0,0 +1,35 @@
+package com.springrest.springrest.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springrest.springrest.entities.Course;
+import com.springrest.springrest.services.CourseService;
+
+@RestController
+public class MyController {
+	
+	@Autowired
+	private CourseService courseService;
+   @GetMapping("/home")
+	public String home() {
+		return "This is home";
+	}
+   
+   //getting the courses
+   
+   @GetMapping("/courses")
+   public List<Course> getCourses(){
+	   return this.courseService.getCourses();
+   }
+   
+   @PostMapping("/course")
+   public Course addCourse(@RequestBody Course course) {
+	   return this.courseService.addCourse(course);
+   }
+}
